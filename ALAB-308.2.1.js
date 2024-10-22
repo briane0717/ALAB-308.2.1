9755ecc14d70372e101bdea9c452011e10110898
Brian <briane0717@gmail.com>
2024-10-22 19:36:25 -0400

    added control flow block into weekCount 2

--- ALAB-308.2.1.js
+++ ALAB-308.2.1.js
@@ -28,3 +28,16 @@ if (plantCount > gardenCapacity * 0.8) {
 weekCount = 2;
 plantCount = initialCount * 2 ** (weekCount - 1);
 console.log(plantCount);
+
+if (plantCount > gardenCapacity * 0.8) {
+  console.log("Prune");
+} else if (
+  plantCount > gardenCapacity * 0.5 &&
+  plantCount < gardenCapacity * 0.8
+) {
+  console.log("Monitor");
+} else if (plantCount < gardenCapacity * 0.5) {
+  console.log("Plant");
+} else {
+  console.log("Everything is fine");
+}
